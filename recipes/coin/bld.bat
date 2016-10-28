7b49af01411e0192d01c647171b4dec60e889f26
looooo <sppedflyer@gmail.com>
2016-10-28 20:25:42 +0200

    DCMAKE_BUILD_TYPE="Release"

--- recipes/coin/bld.bat
+++ recipes/coin/bld.bat
@@ -3,7 +3,8 @@ cd build
 
 cmake .. -G "Ninja" ^
     -DCMAKE_PREFIX_PATH:FILEPATH="%PREFIX%" ^
-    -DCMAKE_INSTALL_PREFIX:FILEPATH="%LIBRARY_PREFIX%"
+    -DCMAKE_INSTALL_PREFIX:FILEPATH="%LIBRARY_PREFIX%" ^
+    -DCMAKE_BUILD_TYPE="Release"
 
 if errorlevel 1 exit 1
 
